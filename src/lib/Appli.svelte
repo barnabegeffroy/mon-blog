f452a4a7b8d41bcd869cee15ebd93bbb2415c93d
Barnabé Geffroy <barnabe.geffroy@ecomail.fr>
2022-05-20 20:27:39 +0200

    finish badge func

--- src/lib/Appli.svelte
+++ src/lib/Appli.svelte
@@ -30,41 +30,58 @@
 
 <div class="app-body" bind:this={element}>
 	<span class="notification">
-		<span class="badge"
-			><img src={base + '/' + file} alt={appli.appname + 'logo'} width="20px" /></span
-		>
-		<img src={base + '/' + file} class="target" alt={appli.appname + 'logo'} on:click={animate} />
-	</span>
-	<h4 class="app-name">{name}</h4>
+		{#if appli.usage == "all"}
+		<span class="badge"><img src={base + '/laptop.svg'} alt="" width="30px" /></span>
+		<span class="badge2"><img src={base + '/phone.svg'} alt="" width="30px" /></span>
+		{:else}
+		<span class="badge"><img src={base + appli.usage == 'desktop' ? '/laptop.svg' : '/phone.svg'} alt="" width="30px" /></span>
+		{/if}
+			<img src={base + '/' + file} class="target" alt={appli.appname + 'logo'} on:click={animate} />
+			<h4 class="app-name">{name}</h4>
+		</span>
 </div>
 
 <style>
 	.notification {
 		position: relative;
+		padding-top: 15px;
+		padding-left: 10px;
 	}
 	.badge {
 		background-color: red;
 		display: flex;
-		align-items: center;
-		justify-content: center;
 		height: 25px;
 		width: 25px;
-		margin-left: 110px;
-		top: 20px;
-		right: 10px;
+		padding: 10px;
+		z-index: 2;
+		left: 95px;
+		bottom: 135px;
+		position: absolute;
+		border-radius: 50%;
+	}
+
+	.badge2 {
+		background-color: red;
+		display: flex;
+		height: 25px;
+		width: 25px;
 		padding: 10px;
 		z-index: 1;
-		position: inherit;
+		left: 65px;
+		bottom: 135px;
+		position: absolute;
 		border-radius: 50%;
 	}
 	.app-body {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		margin: 2%;
+		margin: 10px;
 	}
 	.app-name {
 		margin-top: 1%;
+		width: 120px;
+		text-align: center;
 	}
 
 	.target {
@@ -83,6 +100,9 @@
 		.target {
 			width: 90px;
 			height: 90px;
+		}
+		.app-name{
+			width: 90px;
 		}
 	}
 </style>
